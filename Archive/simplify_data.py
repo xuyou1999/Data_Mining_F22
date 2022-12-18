dc73a953453adee7f2571e5ab93e03d7777d0316
xuyou1999 <yx1711@nyu.edu>
2022-12-18 14:02:23 +0100

    move archive

--- Archive/simplify_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("other_data/data1206.csv")
-print(df.dtypes)
-df = df.loc[:,["time","mode","vehicle_x","vehicle_y", "directionid", "lineid", "speed_meter_second"]]
-df = df[df["lineid"]==95]
-df["vehicle_x"] = df["vehicle_x"].astype(int)
-df["vehicle_y"] = df["vehicle_y"].astype(int)
-df["speed_meter_second"] = df["speed_meter_second"].map('{:,.4f}'.format)
-df.to_csv("other_data/simplified_data.csv",index=False)
